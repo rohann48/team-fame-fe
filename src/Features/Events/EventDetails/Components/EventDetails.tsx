4e82849d2266bc7190cf0c86b68ad4334d2c6586
Laveen Kumar <laveen.kumar@updaptcsr.com>
2024-04-09 18:54:05 +0530

    feat;style fix

--- src/Features/Events/EventDetails/Components/EventDetails.tsx
+++ src/Features/Events/EventDetails/Components/EventDetails.tsx
@@ -5,79 +5,74 @@ import "../SCSS/styles.css";
 
 function EventDetails() {
   return (
-    <div>
-      <div className="events-details-cards-container">
-        <div className="events-details-bg-img-cover"></div>
-        <div className="events-details-cards-and-header-cover">
-          <div className="events-details-cards-header-text">EVENTS</div>
-          <div className="events-details-cards-render-cover">
-            <div className="events-details-cards-left-img-cover">
-              <img
-                className="events-details-cards-left-img"
-                src={images.storeImage}
-                alt="events"
-              />
+    <div className="events-details-cards-container">
+      <div className="events-details-bg-img-cover"></div>
+      <div className="events-details-cards-and-header-cover">
+        <div className="events-details-cards-header-text">EVENTS</div>
+        <div className="events-details-cards-render-cover">
+          <div className="events-details-cards-left-img-cover">
+            <img
+              className="events-details-cards-left-img"
+              src={images.storeImage}
+              alt="events"
+            />
+          </div>
+          <div className="events-details-cards-right-cover">
+            <div className="events-details-cards-header">
+              Marathon event 2023
             </div>
-            <div className="events-details-cards-right-cover">
-              <div className="events-details-cards-header">
-                Marathon event 2023
+            <div className="events-details-cards-description">
+              A MARATHON EVENT IS A LONG-DISTANCE RUNNING RACE WITH AN OFFICIAL
+              DISTANCE OF 42.195 KILOMETERS (26 MILES AND 385 YARDS). THE
+              MARATHON IS ONE OF THE MOST ICONIC AND CHALLENGING ENDURANCE
+              RACES, ATTRACTING RUNNERS FROM ALL AROUND THE WORLD. COMPLETING A
+              MARATHON IS A PROFOUND PERSONAL ACHIEVEMENT THAT INSTILLS A SENSE
+              OF PRIDE, ACCOMPLISHMENT, AND CAMARADERIE AMONG PARTICIPANTS. IT
+              REQUIRES MENTAL TOUGHNESS, DETERMINATION, AND DEDICATION TO
+              OVERCOME THE PHYSICAL AND MENTAL CHALLENGES OF THE RACE.
+            </div>
+            <div className="events-details-cards-date-and-time-cover">
+              <div className="events-details-cards-date">
+                {icons.date}&nbsp; 1 april 2020
               </div>
-              <div className="events-details-cards-description">
-                A MARATHON EVENT IS A LONG-DISTANCE RUNNING RACE WITH AN
-                OFFICIAL DISTANCE OF 42.195 KILOMETERS (26 MILES AND 385 YARDS).
-                THE MARATHON IS ONE OF THE MOST ICONIC AND CHALLENGING ENDURANCE
-                RACES, ATTRACTING RUNNERS FROM ALL AROUND THE WORLD. COMPLETING
-                A MARATHON IS A PROFOUND PERSONAL ACHIEVEMENT THAT INSTILLS A
-                SENSE OF PRIDE, ACCOMPLISHMENT, AND CAMARADERIE AMONG
-                PARTICIPANTS. IT REQUIRES MENTAL TOUGHNESS, DETERMINATION, AND
-                DEDICATION TO OVERCOME THE PHYSICAL AND MENTAL CHALLENGES OF THE
-                RACE.
+              <div className="events-details-cards-time">
+                {icons.clock}&nbsp; 11:20
               </div>
-              <div className="events-details-cards-date-and-time-cover">
-                <div className="events-details-cards-date">
-                  {icons.date}&nbsp; 1 april 2020
-                </div>
-                <div className="events-details-cards-time">
-                  {icons.clock}&nbsp; 11:20
-                </div>
-              </div>
-              <div className="events-details-cards-location">
-                {" "}
-                {icons.location}&nbsp; managlore
-              </div>
-              <div className="events-details-cards-register-btn-cover">
-                <div className="events-details-cards-register-btn">
-                  Register
-                </div>
-              </div>
+            </div>
+            <div className="events-details-cards-location">
+              {" "}
+              {icons.location}&nbsp; managlore
+            </div>
+            <div className="events-details-cards-register-btn-cover">
+              <div className="events-details-cards-register-btn">Register</div>
             </div>
           </div>
         </div>
-        <div className="events-details-former-text-main-cover">
-          <div className="events-details-former-text-top-border">
-            <div className="events-details-former-text-top-border-inner"></div>
-          </div>
-          <div className="events-details-former-text-wrapper">
-            <span>F</span>
-            <span>A</span>
-            <span className="text-r">R</span>
-            <span>M</span>
-            <span>E</span>
-            <span className="text-r">R</span>
-          </div>
-          <div className="events-details-former-text-student-cover">
-            <span>From the students</span>
-            <span>For the students</span>
-            <span>To the students</span>
-          </div>
-          <div className="events-details-former-text-join-btn-cover">
-            <button className="events-details-former-text-join-btn">
-              Join Now
-            </button>
-          </div>
-        </div>
-        <Footer />
       </div>
+      <div className="events-details-former-text-main-cover">
+        <div className="events-details-former-text-top-border">
+          <div className="events-details-former-text-top-border-inner"></div>
+        </div>
+        <div className="events-details-former-text-wrapper">
+          <span>F</span>
+          <span>A</span>
+          <span className="text-r">R</span>
+          <span>M</span>
+          <span>E</span>
+          <span className="text-r">R</span>
+        </div>
+        <div className="events-details-former-text-student-cover">
+          <span>From the students</span>
+          <span>For the students</span>
+          <span>To the students</span>
+        </div>
+        <div className="events-details-former-text-join-btn-cover">
+          <button className="events-details-former-text-join-btn">
+            Join Now
+          </button>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
