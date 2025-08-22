7867d905b9c9d85c1ca8c9a24c346fc590f9d6fb
rohan <rohan@updaptcsr.com>
2025-08-23 01:48:02 +0530

    missing img variable

--- src/Features/ImageVariables.tsx
+++ src/Features/ImageVariables.tsx
@@ -159,7 +159,7 @@ if (process.env.NODE_ENV === "development") {
     eventNewLogo: require("./Assets/Images/eventNewLogo.svg"),
     eventsMarathon: require("./Assets/Images/eventsMarathon.svg"),
     fameNewLogo: require("./Assets/Images/fameNewLogo.png"),
-    shreeLogoNew: require("./Assets/Images/shreeLogoNew.png"),
+    shreeLogoNew: require("./Assets/Images/shreenewlogo.png"),
   };
 }
 
